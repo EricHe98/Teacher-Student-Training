89855156746f1d511f833fd7cd9f500c0daa55b9
EricHe98 <eriqqc@gmail.com>
2017-08-10 20:00:26 -0400

    add dummy prototxt

--- imagenet/alexnet-VGG/load_VGG_weights.py
+++ imagenet/alexnet-VGG/load_VGG_weights.py
@@ -3,9 +3,12 @@ import numpy as np
 
 caffe.set_mode_gpu()
 
+# make sure the train data for the 2 different nets are different (e.g. train for trained_VGG and val for new_VGG
+# because caffe will bug out otherwise
+
 trained_VGG = caffe.Net("Teacher-Student-Training/imagenet/alexnet-VGG/vgg16_original.prototxt", "VGG_ILSVRC_16_layers.caffemodel", caffe.TRAIN)
 
-new_VGG = caffe.Net("Teacher-Student-Training/imagenet/alexnet-VGG/vgg16_train_val.prototxt", caffe.TRAIN)
+new_VGG = caffe.Net("Teacher-Student-Training/imagenet/alexnet-VGG/vgg16_dummy.prototxt", caffe.TRAIN)
 
 # we load the first 4 convolutions
 mapping = {"conv1_1" : "conv_st_1_1",
@@ -20,8 +23,10 @@ for i in range(0, len(mapping)):
 	new_VGG.params[new][1].data[...] = trained_VGG.params[trained][1].data[...]
 	print "mapped %s to %s" % (trained, new)
 
+# remove layers that were not copied over
 for key in new_VGG.params.keys():
 	if key not in mapping.keys():
 		new_VGG.params.pop(key)
+		new_VGG.blobs.pop(key)
 
 new_VGG.save("preloaded_VGG.caffemodel")
