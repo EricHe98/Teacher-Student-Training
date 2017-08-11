a75de415afa1115ba313458cb0d5e269c1bde944
EricHe98 <eriqqc@gmail.com>
2017-08-11 17:42:28 -0400

    modify load_VGG_weights.py

--- imagenet/alexnet-VGG/load_VGG_weights.py
+++ imagenet/alexnet-VGG/load_VGG_weights.py
@@ -8,22 +8,24 @@ caffe.set_mode_gpu()
 
 trained_VGG = caffe.Net("Teacher-Student-Training/imagenet/alexnet-VGG/vgg16_original.prototxt", "VGG_ILSVRC_16_layers.caffemodel", caffe.TRAIN)
 
-new_VGG = caffe.Net("Teacher-Student-Training/imagenet/alexnet-VGG/vgg16_train_val.prototxt", caffe.TRAIN)
+new_VGG = caffe.Net("Teacher-Student-Training/imagenet/alexnet-VGG/vgg16_original2.prototxt", caffe.TRAIN)
 
 # we load the first 4 convolutions
-mapping = {"conv1_1" : "conv_st_1_1",
-		   "conv1_2" : "conv_st_1_2",
-		   "conv2_1" : "conv_st_2_1",
-		   "conv2_2" : "conv_st_2_2",
-		   "conv3_1" : "conv_st_3_1",
-		   "conv3_2" : "conv_st_3_2",
-		   "conv3_3" : "conv_st_3_3",
-		   "conv4_1" : "conv_st_4_1",
-		   "conv4_2" : "conv_st_4_2",
-		   "conv4_3" : "conv_st_4_3",
-		   "conv5_1" : "conv_st_5_1",
-		   "conv5_2" : "conv_st_5_2",
-		   "conv5_3" : "conv_st_5_3",}
+mapping = {"conv1_1" : "conv1_1",
+		   "conv1_2" : "conv1_2",
+		   "conv2_1" : "conv2_1",
+		   "conv2_2" : "conv2_2",
+		   "conv3_1" : "conv3_1",
+		   "conv3_2" : "conv3_2",
+		   "conv3_3" : "conv3_3",
+		   "conv4_1" : "conv4_1",
+		   "conv4_2" : "conv4_2",
+		   "conv4_3" : "conv4_3",
+		   "conv5_1" : "conv5_1",
+		   "conv5_2" : "conv5_2",
+		   "conv5_3" : "conv5_3",
+		   "fc6" : "fc6",
+		   "fc7" : "fc7"}
 
 for i in range(0, len(mapping)):
 	trained = mapping.keys()[i]
